fb90757a28435e38ebc86d88beb5143b77e044c7
unmsm-jcrs <jair.cachay@unmsm.edu.pe>
2024-11-21 02:21:02 -0500

    Creacion de archivos y bases

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routes import cultivos, usuarios, clima, actividades, sesiones
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "*",
+        "http://127.0.0.1:8000"
+        ],  # Poner lo de devtunel
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
+app.include_router(cultivos.router)
+app.include_router(usuarios.router)
+app.include_router(clima.router)
+app.include_router(actividades.router)
+app.include_router(sesiones.router)
