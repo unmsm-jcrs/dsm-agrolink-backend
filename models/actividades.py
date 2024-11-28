8f1a24b006b96432929f75038a89e299c7030eab
unmsm-jcrs <jair.cachay@unmsm.edu.pe>
2024-11-27 23:42:00 -0500

    Correcciones

--- models/actividades.py
+++ models/actividades.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Date, Text
+from sqlalchemy import Column, Integer, Date, Text
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
